bedd03b7b80aa6bbf1f7598c426228d8003e1de7
Lucio <luciocarvalhodev@gmail.com>
2021-12-02 13:55:19 -0300

    header

--- front/js/Order.js
+++
@@ -1,5 +0,0 @@
-class Order {
-    constructor(value, name, phone, description, payment, finished) {
-
-    }
-}
---
+++ front/js/model/Order.js
@@ -0,0 +1,13 @@
+class Order {
+    constructor({ price, name, phone, description, payment, finished }) {
+        this.price = price;
+
+        this.name = name;
+        this.phone = phone;
+
+        this.description = description;
+        this.payment = payment;
+        this.finished = finished;
+    }
+
+}
