f69f32ea2cfc19d550523179cdcbdf6d1e19f095
lucio <luciocarvalhodev@gmail.com>
2022-01-01 09:26:31 -0300

    interface para adicionar pedidos iniciada

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ import Controller from "./js/controller/Controller.js";
 
 const table = document.querySelector(".m_table_orders");
 const modalContainer = document.querySelector(".m_modal-area");
+const overlay = document.getElementById('overlay');
 
 const controller = new Controller(table);
 
@@ -51,26 +52,48 @@ document.querySelector('.h_form').onsubmit = applyFilter;
 document.getElementById('btn-search').onclick = applyFilter;
 document.getElementById('btn-clear').onclick = clearFilters;
 
-document.getElementById('btn-config').onclick = toggleModal;
-document.getElementById('btn-export').onclick = controller.exportData.bind(controller);
-document.getElementById('btn-import').onclick = (e) => {
-    e.preventDefault();
 
-    const elem = document.createElement('input');
-    elem.type = "file";
-    elem.accept = ".yaml";
 
-    elem.onchange = (event) => {
-        event.target.files[0]
-            .text()
-            .then(text => controller.importData(text));
+document.getElementById('btn-config').onclick = (e) => {
+    toggleModal(e);
+    overlay.innerHTML = `
+<div class="m_modal-area_overlay_modal">
+    <h4 class="m_modal-area_overlay_modal_title">Configurações</h4>
+    <div class="m_modal-area_overlay_modal_sec">
+        <input type="button" value="Exportar" id="btn-export"
+            class="m_modal-area_overlay_modal_sec_btn">
+        <input type="button" id="btn-import" class="m_modal-area_overlay_modal_sec_btn" value="Importar"
+            name="file">
+    </div>
+    <div class="m_modal-area_overlay_modal_sec">
+        <input type="button" value="X" id="btn-clear-data" class="m_modal-area_overlay_modal_sec_btn">
+    </div>
+</div>
+    `;
+
+    document.getElementById('btn-export').onclick = controller.exportData.bind(controller);
+    document.getElementById('btn-import').onclick = (e) => {
+        e.preventDefault();
+
+        const elem = document.createElement('input');
+        elem.type = "file";
+        elem.accept = ".yaml";
+
+        elem.onchange = (event) => {
+            event.target.files[0]
+                .text()
+                .then(text => controller.importData(text));
+        };
+        elem.click();
     };
-    elem.click();
-};
-document.getElementById('btn-clear-data').onclick = (e) => {
-    e.preventDefault();
+    document.getElementById('btn-clear-data').onclick = (e) => {
+        e.preventDefault();
 
-    controller.clearData();
+        controller.clearData();
+    };
+
+    overlay.onclick = toggleModal;
 };
 
-document.getElementById('overlay').onclick = toggleModal;
+
+
